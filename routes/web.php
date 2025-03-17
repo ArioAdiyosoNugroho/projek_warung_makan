272aa9512ccb6ebfe45b99e5e267aa3e021d1a7e
Ario adiyoso nugroho <arioadiyoso@gmail.com>
2025-03-17 17:36:47 +0700

    mengupdate cek

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('cek', function () {
+Route::get('/cek', function () {
     return view('cek');
 });
