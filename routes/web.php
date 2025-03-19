04f60fe62339477e4b4123aaee13cc39bf98df16
Ario adiyoso nugroho <arioadiyoso@gmail.com>
2025-03-19 13:40:54 +0700

    menambahkan dan menginstall jetsream

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', function () {
 Route::get('/cekin', function () {
     return view('cek');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
