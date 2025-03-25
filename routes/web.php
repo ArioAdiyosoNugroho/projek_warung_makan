db89aad90b57c3543449348b3d9840673c2cf46a
Ario adiyoso nugroho <arioadiyoso@gmail.com>
2025-03-25 12:38:00 +0700

    menambah sedikit halaman home

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/cekin', function () {
-    return view('cek');
+// Route::get('/cekin', function () {
+//     return view('cek');
+// });
+
+Route::get('/home', function () {
+    return view('Home.index');
 });
 
 Route::middleware([
